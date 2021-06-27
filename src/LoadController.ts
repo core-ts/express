192a61ea00246e25cd270879863aef94a33539ff
minhduc140583 <minhduc140583@gmail.com>
2021-06-27 23:28:30 +0700

    Refactor HealthController and LoadController

--- src/LoadController.ts
+++ src/LoadController.ts
@@ -1,42 +1,11 @@
 import {Request, Response} from 'express';
 import {Attribute, Attributes, Model} from './metadata';
+import {handleError} from './response';
+import {buildId, buildKeys} from './view';
 
 export interface ViewService<T, ID> {
   metadata?(): Model;
   load(id: ID, ctx?: any): Promise<T>;
-}
-export function buildId<T>(req: Request, attrs?: Attribute[]): T {
-  let key = 'id';
-  if (attrs && attrs.length === 1) {
-    const id = req.params[key];
-    if (id && id.length > 0) {
-      return id as any;
-    }
-    key = (attrs[0].name ? attrs[0].name : 'id');
-  }
-  if (!attrs || attrs.length <= 1) {
-    const id = req.params[key];
-    if (!id || id.length === 0) {
-      return null;
-    }
-    return id as any;
-  }
-  const ids: any = {};
-  for (const attr of attrs) {
-    const v = req.params[attr.name];
-    if (!v) {
-      return null;
-    }
-    if (attr.type === 'integer' || attr.type === 'number') {
-      if (isNaN(v as any)) {
-        return null;
-      }
-      ids[attr.name] = parseFloat(v);
-    } else {
-      ids[attr.name] = v;
-    }
-    return ids;
-  }
 }
 function getViewFunc<T, ID>(viewService: ViewService<T, ID> | ((id: ID, ctx?: any) => Promise<T>)): (id: ID, ctx?: any) => Promise<T> {
   if (typeof viewService === 'function') {
@@ -72,27 +41,10 @@ function getKeysFunc<T, ID>(viewService: ViewService<T, ID> | ((id: ID, ctx?: an
   }
   return undefined;
 }
-export function buildKeys(attrs: Attributes): Attribute[] {
-  if (!attrs) {
-    return undefined;
-  }
-  const keys: string[] = Object.keys(attrs);
-  const ats: Attribute[] = [];
-  for (const key of keys) {
-    const attr: Attribute = attrs[key];
-    if (attr) {
-      if (attr.key === true) {
-        const at: Attribute = {name: key, type: attr.type};
-        ats.push(at);
-      }
-    }
-  }
-  return ats;
-}
 export class LoadController<T, ID> {
   protected keys?: Attribute[];
   protected view: (id: ID, ctx?: any) => Promise<T>;
-  constructor(protected log: (msg: string, ctx?: any) => void, viewService: ViewService<T, ID> | ((id: ID, ctx?: any) => Promise<T>), keys?: Attributes|Attribute[]|string[]) {
+  constructor(protected log: (msg: any, ctx?: any) => void, viewService: ViewService<T, ID> | ((id: ID, ctx?: any) => Promise<T>), keys?: Attributes|Attribute[]|string[]) {
     this.load = this.load.bind(this);
     this.view = getViewFunc(viewService);
     this.keys = getKeysFunc(viewService, keys);
@@ -100,7 +52,7 @@ export class LoadController<T, ID> {
   load(req: Request, res: Response) {
     const id = buildId<ID>(req, this.keys);
     if (!id) {
-      return res.status(400).end('Invalid parameters');
+      return res.status(400).end('invalid parameters');
     }
     this.view(id).then(obj => {
       if (obj) {
@@ -108,11 +60,6 @@ export class LoadController<T, ID> {
       } else {
         res.status(404).json(null);
       }
-    }).catch(err => {
-      if (this.log) {
-        this.log(err as any);
-      }
-      res.status(500).send('Internal Server Error');
-    });
+    }).catch(err => handleError(err, res, this.log));
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-
 export * from './health';
 export * from './HealthController';
 export * from './metadata';
+export * from './view';
+export * from './response';
 export * from './LoadController';
