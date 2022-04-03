e88420cd89e8aeab942e2234109db13d17473141
thanhhaile051020 <thanhhaile0510@gmail.com>
2022-04-03 20:03:06 +0700

    update src, lib, package

---
+++ src/client.ts
@@ -0,0 +1,69 @@
+import * as http from 'http';
+import * as https from 'https';
+import { AnyMap, HealthChecker } from './health';
+
+function getHealthSecure(url: string, timeout: number): Promise<AnyMap> {
+  return new Promise((resolve) => {
+    https.get(url, { rejectUnauthorized: false }, (res: any) => {
+      let data = '';
+      res.on('data', (d: any) => {
+        data += d;
+      });
+      res.on('end', () => {
+        resolve({ statusCode: res.statusCode, data, statusMessage: res.statusMessage });
+      });
+    }).on('error', (e: any) => {
+      return { statusCode: 500, statusMessage: e };
+    });
+    setTimeout(() => resolve({ statusCode: 408, statusMessage: 'Time out' }), timeout);
+  });
+}
+function getHealth(url: string, timeout: number): Promise<AnyMap> {
+  return new Promise((resolve) => {
+    http.get(url, (res: any) => {
+      let data = '';
+      res.on('data', (d: any) => {
+        data += d;
+      });
+      res.on('end', () => {
+        resolve({ statusCode: res.statusCode, data, statusMessage: res.statusMessage });
+      });
+    }).on('error', (e: any) => {
+      return { statusCode: 500, statusMessage: e };
+    });
+    setTimeout(() => resolve({ statusCode: 408, statusMessage: 'Time out' }), timeout);
+  });
+}
+export class ClientChecker implements HealthChecker {
+  timeout: number;
+  constructor(private service: string, private url: string, timeout: number) {
+    this.timeout = (timeout ? timeout : 4200);
+    this.check = this.check.bind(this);
+    this.name = this.name.bind(this);
+    this.build = this.build.bind(this);
+  }
+  check(): Promise<AnyMap> {
+    let obj = {} as AnyMap;
+    if (this.url.startsWith('https://')) {
+      return getHealthSecure(this.url, this.timeout).then(
+        r => obj = r
+      );
+    } else {
+      return getHealth(this.url, this.timeout).then(
+        r => obj = r
+      );
+    }
+  }
+  name(): string {
+    return this.service;
+  }
+  build(data: AnyMap, err: any): AnyMap {
+    if (err) {
+      if (!data) {
+        data = {} as AnyMap;
+      }
+      data['error'] = err;
+    }
+    return data;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ export {Controller as LowCodeController};
 export {Service as LowCodeService};
 
 export * from './health';
+export * from './client';
 export * from './HealthController';
 export * from './LogController';
 export * from './log';
@@ -43,15 +44,37 @@ export * from './GenericSearchController';
 export * from './LowCodeController';
 
 export interface AccessConfig {
-  origin: string;
-  credentials: string;
-  methods: string;
-  headers: string;
+  origin?: string | string[];
+  credentials?: string | string[];
+  methods?: string | string[];
+  headers: number | string | ReadonlyArray<string>;
 }
 export type AccessControlAllowConfig = AccessConfig;
 export function allow(access: AccessConfig): (req: Request, res: Response, next: NextFunction) => void {
+  const ao = access.origin;
+  if (typeof ao === 'string') {
+    return (req: Request, res: Response, next: NextFunction) => {
+      res.header('Access-Control-Allow-Origin', access.origin);
+      res.header('Access-Control-Allow-Credentials', access.credentials);
+      res.header('Access-Control-Allow-Methods', access.methods);
+      res.setHeader('Access-Control-Allow-Headers', access.headers);
+      next();
+    };
+  } else if (Array.isArray(ao) && ao.length > 0) {
+    return (req: Request, res: Response, next: NextFunction) => {
+      const origin = req.headers.origin;
+      if (origin) {
+        if (ao.includes(origin)) {
+          res.setHeader('Access-Control-Allow-Origin', origin);
+        }
+      }
+      res.header('Access-Control-Allow-Credentials', access.credentials);
+      res.header('Access-Control-Allow-Methods', access.methods);
+      res.setHeader('Access-Control-Allow-Headers', access.headers);
+      next();
+    };
+  }
   return (req: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', access.origin);
     res.header('Access-Control-Allow-Credentials', access.credentials);
     res.header('Access-Control-Allow-Methods', access.methods);
     res.setHeader('Access-Control-Allow-Headers', access.headers);
